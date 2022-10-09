134d484c10d673e3ca935de7248926b889f8cfa3
Anibal Lopez <aniballopez0708@gmail.com>
2022-10-09 12:53:40 -0400

    new file:   views/css/principal.css 	modified:   views/principal.php

--- views/principal.php
+++ views/principal.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
-<html lang="eng">
-    <meta chartset = "UTF-8">
-    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QUELOQUESO</title>
-<head></head>
+    <link rel="stylesheet" href="./views/css/principal.css">
+</head>
+
 <body>
-    <h1>pagina principal</h1>
+    <div id="contenedor">
+        <div class="titulo">
+            <h1>Empresa XYZ</h1>
+        </div>
+        <div class="menu">
+            <h2>Planifica tus reuniones</h2>
+            <p>Organiza, crea, expresa tus ideas, expon tus puntos.</p>
+            <button class="registro">Registrarse</button>
+            <button class="inicio">Iniciar Sesion</button>
+        </div>
+    </div>
+    <img src="https://images.unsplash.com/photo-1544654935-036f583c952a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjkzfHxjYWxlbmRhcmlvc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60">
 </body>
-<html>
+
+</html>
