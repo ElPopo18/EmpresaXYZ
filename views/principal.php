ac27a2ba1c6a5fe430dedf9c1f8abfe73a4664a7
jesustt15 <114711868+jesustt15@users.noreply.github.com>
2022-10-08 21:03:09 -0400

    Add files via upload

---
+++ views/principal.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <meta chartset = "UTF-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    <title>QUELOQUESO</title>
+<head></head>
+<body>
+    <h1>pagina principal</h1>
+</body>
+<html>
