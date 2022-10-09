7a29a1c39284ccc0668caf69ef0432d850acc9ca
jesustt15 <114711868+jesustt15@users.noreply.github.com>
2022-10-08 21:04:08 -0400

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('URL', 'http://localhost:8080/pruebasphp/');
+
+define('HOST', 'localhost');
+define('DB', 'consorcio');
+define('USER', 'root');
+define('PASSWORD', "root");
+define('CHARSET', 'utf8mb4');
+
+?>
