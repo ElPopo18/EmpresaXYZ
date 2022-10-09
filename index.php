6f28c8b15f67a80615175fbd42f01d297fa162ab
jesustt15 <114711868+jesustt15@users.noreply.github.com>
2022-10-08 21:02:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("config/config.php");
+require_once("controllers/index.php");
+if(isset($_GET['n'])){
+    if(method_exists("ModeloController",$_GET['n'])){
+        ModeloController::{$_GET['n']}();
+    }
+}
+else{
+    ModeloController::principal();
+    }
+?>
+
