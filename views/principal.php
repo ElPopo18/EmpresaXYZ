4d213340ca974d6e85c6c4bf985c8eeedc2ed7ae
Anibal Lopez <aniballopez0708@gmail.com>
2022-10-09 13:08:46 -0400

    cambios

--- views/principal.php
+++ views/principal.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QUELOQUESO</title>
+    <title>Empresa XYZ</title>
     <link rel="stylesheet" href="./views/css/principal.css">
+    <link rel="icon" href="./views/css/calendario.png">
 </head>
 
 <body>
