c300d7ca14e1b48ec953964518256a759f105fc1
Aniballopez7 <aniballopez0708@gmail.com>
2022-10-15 02:20:14 -0400

    Update consorcio.sql

--- db/consorcio.sql
+++ db/consorcio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-10-2022 a las 10:38:45
+-- Tiempo de generación: 15-10-2022 a las 08:17:31
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -26,9 +26,56 @@ SET time_zone = "+00:00";
 --
 -- Estructura de tabla para la tabla `empresa`
 --
+
 CREATE TABLE `empresa` (
-  `id` int(11) NOT NULL,
-  `nombre_empresa` varchar(100) NOT NULL
+  `rif` varchar(25) NOT NULL,
+  `nombre_empresa` varchar(100) NOT NULL,
+  `password` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `empresa`
+--
+
+INSERT INTO `empresa` (`rif`, `nombre_empresa`, `password`) VALUES
+('asdas', 'adsasd', 0),
+('sdfhjks4454', '', 988);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresa_has_socio`
+--
+
+CREATE TABLE `empresa_has_socio` (
+  `id_empresoc` int(25) NOT NULL,
+  `id_empresa` int(25) NOT NULL,
+  `id_socio` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `punto`
+--
+
+CREATE TABLE `punto` (
+  `id_punto` int(65) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `descision` varchar(25) NOT NULL,
+  `archivo` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reunion`
+--
+
+CREATE TABLE `reunion` (
+  `id_reunion` int(25) NOT NULL,
+  `id_punto` int(30) NOT NULL,
+  `fecha` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -41,30 +88,8 @@ CREATE TABLE `socio` (
   `ced_socio` int(10) NOT NULL,
   `nombre_soc` varchar(25) NOT NULL,
   `apellido_soc` varchar(25) NOT NULL,
-  `username` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `punto`
---
-
-CREATE TABLE `punto`  (
-  `id_punto` int(65) NOT NULL,
-  `descripcion` varchar(250) NOT NULL,
-  `descision` varchar(25) NOT NULL,
-  `archivo` varchar(25)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `reunion`
---
-
-CREATE TABLE `reunion`  (
-  `id_reunion` int(25) NOT NULL,
-  `id_punto` int(30) NOT NULL,
-  `fecha` datetime  
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -73,21 +98,9 @@ CREATE TABLE `reunion`  (
 -- Estructura de tabla para la tabla `socio_has_punto`
 --
 
-CREATE TABLE `socio_has_punto`  (
+CREATE TABLE `socio_has_punto` (
   `id` int(25) NOT NULL,
   `id_punto` int(30) NOT NULL,
-  `id_socio` int(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `empresa_has_socio`
---
-
-CREATE TABLE `empresa_has_socio`  (
-  `id_empresoc` int(25) NOT NULL,
-  `id_empresa` int(25) NOT NULL,
   `id_socio` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -99,7 +112,7 @@ CREATE TABLE `empresa_has_socio`  (
 -- Indices de la tabla `empresa`
 --
 ALTER TABLE `empresa`
-  ADD PRIMARY KEY (`id_empresa`);
+  ADD PRIMARY KEY (`rif`);
 
 --
 -- Indices de la tabla `socio`
@@ -108,101 +121,16 @@ ALTER TABLE `socio`
   ADD PRIMARY KEY (`ced_socio`);
 
 --
--- Indices de la tabla `punto`
---
-ALTER TABLE `punto`
-    ADD PRIMARY KEY (`id_punto`),
-
---
--- Indices de la tabla `reunion`
---
-ALTER TABLE `reunion`
-    ADD PRIMARY KEY (`id_reunion`),
-    ADD KEY `id_punto` (`id_punto`);
-
---
--- Indices de la tabla `punto`
---
-ALTER TABLE `punto`
-    ADD PRIMARY KEY (`id_punto`);
---
--- Indices de la tabla `socio_has_punto`
---
-ALTER TABLE `socio_has_punto`
-    ADD PRIMARY KEY (`id`),
-    ADD KEY `id_punto` (`id_punto`),
-    ADD KEY `id_socio` (`id_socio`);         
---
--- Indices de la tabla `empresa_has_socio`
---
-ALTER TABLE `empresa_has_socio`
-    ADD PRIMARY KEY (`id_empresoc`),
-    ADD KEY `id_empresa` (`id_empresa`),
-    ADD KEY `id_socio` (`id_socio`); 
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `empresa`
+-- AUTO_INCREMENT de la tabla `socio`
 --
-ALTER TABLE `empresa`
-  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `punto`
---
-ALTER TABLE `punto`
-  MODIFY `id_punto` int(65) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `reunion`
---
-ALTER TABLE `reunion`
-  MODIFY `id_reunion` int(25) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `socio_has_punto`
---
-ALTER TABLE `socio_has_punto`
-  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `empresa_has_socio`
---
-ALTER TABLE `socio_has_punto`
-  MODIFY `id_empresasoc` int(25) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `reunion`
---
-ALTER TABLE `reunion`
-  ADD CONSTRAINT `reunion_ibfk_1` FOREIGN KEY (`id_punto`) REFERENCES `punto` (`id_punto`);
-
---
--- Filtros para la tabla `socio_has_punto`
---
-ALTER TABLE `socio_has_punto`
-  ADD CONSTRAINT `socio_has_punto_ibfk_1` FOREIGN KEY (`id_punto`) REFERENCES `punto` (`id_punto`),
-  ADD CONSTRAINT `socio_has_punto_ibfk_2` FOREIGN KEY (`id_socio`) REFERENCES `socio` (`ced_socio`);
-
---
--- Filtros para la tabla `empresa_has_socio`
---
-ALTER TABLE `socio_has_punto`
-  ADD CONSTRAINT `empresa_has_socio_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`),
-  ADD CONSTRAINT `empresa_has_socio_ibfk_2` FOREIGN KEY (`id_socio`) REFERENCES `socio` (`ced_socio`);
+ALTER TABLE `socio`
+  MODIFY `ced_socio` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6466;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
-
-
