93b61e155ffea3184cd9b13a1914b4f78603d92e
Aniballopez7 <aniballopez0708@gmail.com>
2022-10-31 10:19:42 -0400

    cambios
    interfaz

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,6 +1,8 @@
-<?php
+<?php 
 
-$usuario = $_GET['username'];
+session_start();
+
+$username = $_GET['username'];
 
 ?>
 <!DOCTYPE html>
@@ -10,35 +12,36 @@ $usuario = $_GET['username'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EmpresaXYZ - Inicio</title>
+    <title>Dashboard</title>
     <link rel="icon" href="views/img/calendario.png">
     <link rel="stylesheet" href="views/css/indexAdmin.css">
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-solid-rounded/css/uicons-solid-rounded.css'>
 </head>
 
 <body>
     <div id="container">
         <header id="header">
             <h1 class="titulo"><img src="views/img/calendario.png" class="logo">EmpresaXYZ</h1>
-            <nav id="navbar">
-                <ul>
-                    <li>Bienvenido <?php echo $usuario ?></li>
-                    <li><a href="index.php?n=principal">posible opc</a></li>
-                    <li><a href="index.php?n=principal">Cerrar Sesion</a></li>
-                </ul>
-            </nav>
-        </header>
-        <div id="contenido">
             <aside class="lateral">
                 <ul>
-                    <li><a>Inicio</a></li>
+                    <li><a>Dashboard</a></li>
                     <li><a>Calendario</a></li>
                     <li><a>Reuniones</a></li>
                     <li><a>Empresas</a></li>
                     <li><a>Socios</a></li>
-                    <li><a>Configuracion</a></li>
                 </ul>
             </aside>
-            <div class="calendario">
+        </header>
+        <div id="contenido"> 
+            <nav id="navbar">
+                <ul>
+                    <li><a href="index.php?n=principal"><i class="fi fi-rr-settings"></i></a></li>
+                    <li class="margin-right"><a href="index.php?n=principal"><i class="fi fi-sr-exit"></i></a></li>
+                    <li class="ajustar"><img src="https://i.scdn.co/image/ab67616d00001e0249d694203245f241a1bcaa72"><span class="username"><?php echo $username?></span></li>
+                </ul>
+            </nav>
+            <div class="contenido__pagina">
                 <article class="proyects">
                     <div class="image-wrap">
                         <img src="https://via.placeholder.com/250x180">
