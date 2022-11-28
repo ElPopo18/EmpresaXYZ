13dc0f07dcf4a9208acb4e50caf5750c68594ab8
jesustt15 <jesustoussaint08@gmail.com>
2022-11-28 17:17:03 -0400

    Merge branch 'anibal' of https://github.com/ElPopo18/EmpresaXYZ into anibal

--- views/login.php
+++ views/login.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inicio de socio</title>
+  <title>Iniciar Sesión</title>
   <link rel="icon" href="views/img/calendario.png">
   <link rel="stylesheet" href="views/css/login.css">
   <script src="https://kit.fontawesome.com/1c51e264f0.js" crossorigin="anonymous"></script>
